e6eed86944048572371178ef4b851e8959222ab9
BuyuanCui <alexcui1994@gmail.com>
2022-06-08 16:28:00 -0700

    zh itn test files

--- tests/nemo_text_processing/zh/text.py
+++ tests/nemo_text_processing/zh/text.py
@@ -6,5 +6,5 @@ inv.inverse_normalize('一二三',verbose=True) #123
 string='一二三' # in tokenize_and_classify.py
 def tokenize(arg):
     return [c for c in arg]
-
+#tokenize mandarin, so instead of a one string, tokenize by character, one by one so it will work
 print(tokenize(string))
