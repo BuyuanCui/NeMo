1c3f52ee450900ec6334096c9ec0fecd854ac096
BuyuanCui <alexcui1994@gmail.com>
2022-07-06 11:38:55 -0700

    zhitn ordinal verbalizer

--- nemo_text_processing/inverse_text_normalization/zh/verbalizers/ordinal.py
+++ nemo_text_processing/inverse_text_normalization/zh/verbalizers/ordinal.py
@@ -13,7 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from nemo_text_processing.inverse_text_normalization.zh.graph_utils import NEMO_NOT_QUOTE, NEMO_SIGMA, GraphFst, delete_space
+from nemo_text_processing.inverse_text_normalization.zh.graph_utils import NEMO_NOT_QUOTE, GraphFst, delete_space, NEMO_DIGIT
+from nemo_text_processing.inverse_text_normalization.zh.taggers.cardinal import CardinalFst
 
 try:
     import pynini
@@ -23,42 +24,8 @@ try:
 except (ModuleNotFoundError, ImportError):
     PYNINI_AVAILABLE = False
 
-
 class OrdinalFst(GraphFst):
-    """
-    Finite state transducer for verbalizing ordinal, e.g.
-       ordinal { integer: "13" } -> 13th
-    """
-
     def __init__(self):
         super().__init__(name="ordinal", kind="verbalize")
-        graph = (
-            pynutil.delete("integer:")
-            + delete_space
-            + pynutil.delete("\"")
-            + pynini.closure(NEMO_NOT_QUOTE, 1)
-            + pynutil.delete("\"")
-        )
-        convert_eleven = pynini.cross("11", "第十一")
-        convert_twelve = pynini.cross("12", "第十二")
-        convert_thirteen = pynini.cross("13", "第十三")
-        convert_one = pynini.cross("1", "第一")
-        convert_two = pynini.cross("2", "第二")
-        convert_three = pynini.cross("3", "第三")
-        convert_rest = pynutil.insert("th", weight=0.01)
-
-        suffix = pynini.cdrewrite(
-            convert_eleven
-            | convert_twelve
-            | convert_thirteen
-            | convert_one
-            | convert_two
-            | convert_three
-            | convert_rest,
-            "",
-            "[EOS]",
-            NEMO_SIGMA,
-        )
-        graph = graph @ suffix
-        delete_tokens = self.delete_tokens(graph)
-        self.fst = delete_tokens.optimize()
+        graph_integer = (pynutil.delete("integer:") + delete_space + pynutil.delete("\"") + pynutil.delete(NEMO_DIGIT,1) + pynutil.delete("\""))
+        
