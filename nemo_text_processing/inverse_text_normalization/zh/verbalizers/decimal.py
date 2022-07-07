b93430388b5ff7bf9638132edbc076da62aebadf
BuyuanCui <alexcui1994@gmail.com>
2022-07-07 09:44:35 -0700

    zhitn decimal verbalizer

--- nemo_text_processing/inverse_text_normalization/zh/verbalizers/decimal.py
+++ nemo_text_processing/inverse_text_normalization/zh/verbalizers/decimal.py
@@ -22,16 +22,36 @@ try:
 except (ModuleNotFoundError, ImportError):
     PYNINI_AVAILABLE = False
 
+from nemo_text_processing.inverse_text_normalization.zh.utils import get_abs_path
+from nemo_text_processing.inverse_text_normalization.zh.taggers.decimal import DecimalFst
+from nemo_text_processing.inverse_text_normalization.zh.graph_utils import (
+    NEMO_DIGIT,
+    GraphFst,
+    delete_extra_space,
+    delete_space,
+    NEMO_NON_BREAKING_SPACE,
+    NEMO_NOT_QUOTE,
+)
 
 class DecimalFst(GraphFst):
-    """
-    Finite state transducer for verbalizing decimal, e.g.
-        decimal { negative: "true" integer_part: "12"  fractional_part: "5006" quantity: "billion" } -> -12.5006 billion
-    """
-
     def __init__(self):
-        super().__init__(name="decimal", kind="verbalize")
-        optionl_sign = pynini.closure(pynini.cross("negative: \"true\"", "-") + delete_space, 0, 1)
+        super().__init__(name="decimal",kind="verbalize")
+        exactly_three_digits = NEMO_DIGIT **3
+        at_most_three_digits = pynini.closure(NEMO_DIGIT,1,3) 
+        
+        space_every_three_integer = (
+            at_most_three_digits + (pynutil.insert(NEMO_NON_BREAKING_SPACE) + exactly_three_digits).closure()
+        )
+        space_every_three_decimal = (
+            pynini.accep(".") 
+            + (exactly_three_digits + pynutil.insert(NEMO_NON_BREAKING_SPACE)).closure()
+            + at_most_three_digits
+        )
+        group_by_threes = space_every_three_integer | space_every_three_decimal
+        self.group_by_threes = group_by_threes
+        
+        optional_sign = pynini.closure(pynini.cross("negative: \"true\"", "-") + delete_space, 0, 1)
+        
         integer = (
             pynutil.delete("integer_part:")
             + delete_space
@@ -39,26 +59,33 @@ class DecimalFst(GraphFst):
             + pynini.closure(NEMO_NOT_QUOTE, 1)
             + pynutil.delete("\"")
         )
+    
+        integer = integer @ group_by_threes
         optional_integer = pynini.closure(integer + delete_space, 0, 1)
+        
         fractional = (
-            pynutil.insert(".")
+            pynutil.insert(".") 
             + pynutil.delete("fractional_part:")
             + delete_space
             + pynutil.delete("\"")
             + pynini.closure(NEMO_NOT_QUOTE, 1)
             + pynutil.delete("\"")
         )
-        optional_fractional = pynini.closure(fractional + delete_space, 0, 1)
+        fractional = fractional @ group_by_threes
+        optional_fractional = pynini.closure(fractional + delete_space,0,1)
+        
         quantity = (
             pynutil.delete("quantity:")
-            + delete_space
-            + pynutil.delete("\"")
-            + pynini.closure(NEMO_NOT_QUOTE, 1)
+            + delete_space + pynutil.delete("\"")
+            + pynini.closure(NEMO_NOT_QUOTE,1)
             + pynutil.delete("\"")
         )
+        
         optional_quantity = pynini.closure(pynutil.insert(" ") + quantity + delete_space, 0, 1)
-        graph = optional_integer + optional_fractional + optional_quantity
-        self.numbers = graph
-        graph = optionl_sign + graph
+        
+        graph = (optional_integer + optional_fractional + optional_quantity).optimize()
+        self.numebrs = graph
+        graph = optional_sign + graph
         delete_tokens = self.delete_tokens(graph)
         self.fst = delete_tokens.optimize()
+        
